2d00e2814e206521272328ed01d9b05cc52db8cc
Kieran Goodary <kieran@digitalindustria.com>
2020-03-14 18:33:57 +0000

    Updated ingester function.

--- api/functions/lib/ingest.js
+++ api/functions/lib/ingest.js
@@ -1,9 +1,38 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const geohash = require('ngeohash');
 
-const request = functions.https.onRequest((request, response) => {
- response.send({
-   body: request.body,
-   query: request.query,
+let db = admin.firestore();
+
+const request = functions.https.onRequest(async (request, response) => {
+  console.log('Incoming data:', {
+    body: request.body,
+    query: request.query,
+  });
+
+  const hotspotReference = await db.collection('hotspots').doc().set({
+    anonymousUserId: request.body.anonymousUserId,
+    geopoint: new admin.firestore.GeoPoint(
+      request.body.latitude,
+      request.body.longitude
+    ),
+    geohash: geohash.encode(
+      request.body.latitude,
+      request.body.longitude
+    ),
+    hotspotLevel: request.body.level,
+    notes: [],
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    updatedAt: admin.firestore.FieldValue.serverTimestamp()
+  })
+  .then((writeResult) => {
+    console.log('Successfully wrote hotspot:', writeResult);
+  });
+
+console.log('Ingestion complete.')
+
+ return response.send({
+   hotspotReference
  });
 });
 
