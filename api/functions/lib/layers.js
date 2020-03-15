ed871b1d2614b8daa442b6b3ab3ff92d31713673
Kieran Goodary <kieran@digitalindustria.com>
2020-03-15 14:39:36 +0000

    Correcting layers GET endpoint.

--- api/functions/lib/layers.js
+++ api/functions/lib/layers.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const mappedLayerData = [];
+let mappedLayerData = [];
 
 try {
   admin.initializeApp();
