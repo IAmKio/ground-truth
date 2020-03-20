a12bd60830f29156d058f44f9f9b240b064a10b6
Kieran Goodary <kieran@digitalindustria.com>
2020-03-20 13:26:58 +0000

    Updating licenses.

--- api/functions/lib/ingest.js
+++ api/functions/lib/ingest.js
@@ -1,4 +1,4 @@
-// Copyright 2020 k
+// Copyright 2020 Kieran Goodary, Digital Industria Ltd
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
