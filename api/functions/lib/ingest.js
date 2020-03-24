1ebd35ca44e6886933a8cdbcd9a9174958073fea
Kieran Goodary <kieran@digitalindustria.com>
2020-03-24 16:21:04 +0000

    Adding CORS middleware.

--- api/functions/lib/ingest.js
+++ api/functions/lib/ingest.js
@@ -15,6 +15,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const geohash = require('ngeohash');
+const cors = require('cors')({
+  origin: true
+});
 
 try {
   admin.initializeApp();
@@ -31,33 +34,36 @@ const request = functions.https.onRequest(async (request, response) => {
     query: request.query,
   });
 
-  const hotspotReference = await db.collection('hotspots').doc().set({
-    anonymousUserId: request.body.anonymousUserId,
-    geopoint: new admin.firestore.GeoPoint(
-      parseFloat(request.body.latitude),
-      parseFloat(request.body.longitude)
-    ),
-    geohash: geohash.encode(
-      parseFloat(request.body.latitude),
-      parseFloat(request.body.longitude)
-    ),
-    layerId: request.body.layerId,
-    notes: [],
-    createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    updatedAt: admin.firestore.FieldValue.serverTimestamp()
-  })
-  .then((writeResult) => {
-    console.log('Successfully wrote hotspot:', writeResult);
+  return cors(req, res, () => {
+    const hotspotReference = await db.collection('hotspots').doc().set({
+      anonymousUserId: request.body.anonymousUserId,
+      geopoint: new admin.firestore.GeoPoint(
+        parseFloat(request.body.latitude),
+        parseFloat(request.body.longitude)
+      ),
+      geohash: geohash.encode(
+        parseFloat(request.body.latitude),
+        parseFloat(request.body.longitude)
+      ),
+      layerId: request.body.layerId,
+      notes: [],
+      observed: false,
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      updatedAt: admin.firestore.FieldValue.serverTimestamp()
+    })
+    .then((writeResult) => {
+      console.log('Successfully wrote hotspot:', writeResult);
+    });
+
+    db.collection('statistics').doc('hotspots')
+      .update("count", admin.firestore.FieldValue.increment(1));
+
+    console.log('Ingestion complete.')
+
+    res.send({
+      hotspotReference
+    });
   });
-
-db.collection('statistics').doc('hotspots')
-  .update("count", admin.firestore.FieldValue.increment(1));
-
-console.log('Ingestion complete.')
-
- return response.set({ 'Access-Control-Allow-Origin': '*' }).send({
-   hotspotReference
- });
 });
 
 module.exports = request;
